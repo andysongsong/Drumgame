42c43035956df155461ca694e6815ec21256bf8c
andysongsong <sxj930916@gmai.com>
2022-02-01 21:55:47 -0800

    add sound with switch statement

--- index.js
+++ index.js
@@ -3,8 +3,47 @@ numberOfDrumButtons = document.querySelectorAll(".drum").length
 for (var i = 0; i< numberOfDrumButtons; i++) {
 
  document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-  let audio = new Audio('sounds/tom-1.mp3');
-  audio.play()  ;
-  });// what to do when click detected.
+   buttonInnerHTML = this.innerHTML;
+
+   switch (buttonInnerHTML) {
+     case "w":
+       tom1 = new Audio("sounds/tom-1.mp3");
+       tom1.play();
+       break;
+
+     case "a":
+       tom2 = new Audio("sounds/tom-2.mp3");
+       tom2.play();
+       break;
+     
+     case "s":
+       tom3 = new Audio("sounds/tom-3.mp3");
+       tom3.play();
+       break;
+     
+     case "d":
+       tom4 = new Audio("sounds/tom-4.mp3");
+       tom4.play();
+       break;
+      
+     case "j":
+       snare = new Audio("sounds/snare.mp3");
+       snare.play();
+       break;
+
+     case "k":
+       crash = new Audio("sounds/crash.mp3");
+       crash.play();
+       break;
+      
+     case "l":
+       kick = new Audio("sounds/kick-bass.mp3");
+       kick.play();
+       break
+   
+     default: console.log(buttonInnerHTML)
+       
+   }
+});// what to do when click detected.
 }
 
